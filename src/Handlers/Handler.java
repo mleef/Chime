a08563b2a3f8cfcc7d1d420bf921223a826c8e04
Marc Leef <leefmarc@gmail.com>
2015-11-02 17:15:44 -0500

    Added Handler.java

---
+++ src/Handlers/Handler.java
@@ -0,0 +1,17 @@
+package Handlers;
+
+import DataStructures.ChannelMap;
+import DataStructures.TelevisionMap;
+import Messaging.Registration;
+
+import java.net.Socket;
+
+/**
+ * Created by marcleef on 11/2/15.
+ */
+public abstract class Handler implements Runnable {
+
+    public void run() {
+
+    }
+}
