c44700a7c0efb9e64aaa022ba62f28c7e5876dc8
Marc Leef <leefmarc@gmail.com>
2015-11-04 20:42:27 -0500

    Began adding some simple tests for the channel map.

--- src/DataStructures/ChannelMap.java
+++ src/DataStructures/ChannelMap.java
@@ -4,6 +4,7 @@ import TV.Channel;
 import TV.Television;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -20,6 +21,16 @@ public final class ChannelMap extends ConcurrentHashMap<Channel, Set<Television>
         super();
     }
 
+    /**
+     * Adds new associated channel set.
+     * @param channel Channel to initialize in map.
+     * @return Updated set of television objects.
+     **/
+    public Set<Television> addChannel(Channel channel) {
+        this.put(channel, new HashSet<Television>());
+        return this.get(channel);
+    }
+
 
     /**
      * Adds television to channel watch set.
@@ -28,9 +39,14 @@ public final class ChannelMap extends ConcurrentHashMap<Channel, Set<Television>
      * @return Updated set of television objects.
      **/
     public Set<Television> putTV(Channel channel, Television television) {
+        // Check for channel existence
         if(!this.containsKey(channel)) {
             return null;
         } else {
+            // Overwrite existing version if it exists
+            if(this.get(channel).contains(television)) {
+                this.get(channel).remove(television);
+            }
             this.get(channel).add(television);
             return this.get(channel);
         }
@@ -43,12 +59,32 @@ public final class ChannelMap extends ConcurrentHashMap<Channel, Set<Television>
      * @return Updated set of television objects.
      **/
     public Set<Television> removeTV(Channel channel, Television television) {
+        // Check for channel existence
         if(!this.containsKey(channel)) {
             return null;
         } else {
-            this.get(channel).remove(television);
+            // Remove television if it exists in teh given channel
+            if(this.get(channel).contains(television)) {
+                this.get(channel).remove(television);
+            }
             return this.get(channel);
         }
+    }
+
+    /**
+     * Sums all current users.
+     * @return Total number of current users.
+     **/
+    public int getTotalViewers() {
+        int totalViewers = 0;
+
+        // Sum length all viewer sets
+        for(Channel channel : this.keySet()) {
+            totalViewers += this.get(channel).size();
+        }
+
+        return totalViewers;
+
     }
 
 
--- test/DataStructures/ChannelMapTest.java
+++ test/DataStructures/ChannelMapTest.java
@@ -1,5 +1,8 @@
 package DataStructures;
 
+import TV.Channel;
+import TV.Television;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,14 +11,70 @@ import static org.junit.Assert.*;
  * Created by marcleef on 11/2/15.
  */
 public class ChannelMapTest {
+    private ChannelMap channelMap;
+    private Channel c1;
+    private Channel c2;
+    private Channel c3;
+    private Channel c4;
 
-    @Test
-    public void testPutTV() throws Exception {
+
+    @Before
+    public void setUp() throws Exception {
+        this.channelMap = new ChannelMap();
+        this.c1 = new Channel("1");
+        this.c2 = new Channel("2");
+        this.c3 = new Channel("3");
+        this.c4 = new Channel("4");
+        channelMap.addChannel(c1);
+        channelMap.addChannel(c2);
+        channelMap.addChannel(c3);
+        channelMap.addChannel(c4);
 
     }
 
     @Test
-    public void testRemoveTV() throws Exception {
+    public void testPutTV() throws Exception {
+        channelMap.putTV(c1, new Television("tv-1"));
+        channelMap.putTV(c1, new Television("tv-2"));
+        channelMap.putTV(c1, new Television("tv-3"));
+        channelMap.putTV(c1, new Television("tv-4"));
 
+        // Check that all TVs populated properly
+        assertEquals(channelMap.get(c1).size(), 4);
+
+        // Check that nothing was added to other channels
+        assertEquals(channelMap.get(c2).size(), 0);
+        assertEquals(channelMap.get(c3).size(), 0);
+        assertEquals(channelMap.get(c4).size(), 0);
+
+        // Check that accessing nonexistent channel returns null
+        assertNull(channelMap.get(new Channel("5")));
+    }
+
+    @Test
+    public void testRemoveTV() throws Exception {
+        Television tv1 = new Television("tv-1");
+        Television tv2 = new Television("tv-2");
+        Television tv3 = new Television("tv-3");
+        Television tv4 = new Television("tv-4");
+
+        channelMap.putTV(c1, tv1);
+        channelMap.putTV(c1, tv2);
+        channelMap.putTV(c1, tv3);
+        channelMap.putTV(c1, tv4);
+
+        channelMap.removeTV(c1, tv1);
+        channelMap.removeTV(c1, tv2);
+        channelMap.removeTV(c1, tv3);
+        channelMap.removeTV(c1, tv4);
+
+        // Check that all tv objects were removed
+        assertEquals(channelMap.get(c1).size(), 0);
+
+        // Check that invalid channels return null
+        assertNull(channelMap.removeTV(new Channel("5"), tv1));
+
+        // Check that trying to remove nonexistent TV doesn't error
+        assertNotNull(channelMap.removeTV(c1, new Television("tv-5")));
     }
 }
