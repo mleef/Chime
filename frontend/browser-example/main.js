d47d0b64f110f62a542f430202af83cdccffb893
Marc Leef <leefmarc@gmail.com>
2015-12-08 00:16:13 -0500

    Worked on chat application.

--- frontend/browser-example/main.js
+++ frontend/browser-example/main.js
@@ -1,16 +1,17 @@
 var socket;
 var ENDPOINT = 'http://ec2-54-152-59-214.compute-1.amazonaws.com:4567';
 var GET_CHANNELS = ENDPOINT + '/channel/list';
-var curChannel = null;
+var curChannel = "1";
 var prevChannel = null;
 window.onload = function () {
     // TODO:: Do your initialization job
+    var id = prompt("Please enter a username for the chat", "username");
 
     var registration = {
         registrationMessage: {
             previousChannel: null,
             newChannel: {id: "1"},
-            television: {id: "Web TV"}
+            television: {id: id}
         }
     };
     var chime1 = {
@@ -35,7 +36,7 @@ window.onload = function () {
     socket = new WebSocket('ws://ec2-54-152-59-214.compute-1.amazonaws.com:4445');
     socket.onopen = function () {
         console.log("connection opened.");
-        socket.send(JSON.stringify(registration));
+        //socket.send(JSON.stringify(registration));
         //socket.send(JSON.stringify(chime1));
         //socket.send(JSON.stringify(chime2));
     }
@@ -44,18 +45,9 @@ window.onload = function () {
         console.log("response received");
         messageObj = JSON.parse(message.data);
         console.log(messageObj);
-        $("marquee").append(messageObj.message);
+        $("marquee").append(' ' + messageObj.message);
     }
-    $.get( GET_CHANNELS, function( data ) {
-        var list = JSON.parse(data);
-        for(var i = 0; i < list.length; i++) {
-            $('#channelList')
-                .append($("<option></option>")
-                    .text(list[i].id));
-        }
-    });
-
-    setInterval(function() {
+    var updateChannels = function() {
         $.get( GET_CHANNELS, function( data ) {
             var list = JSON.parse(data);
             for(var i = 0; i < list.length; i++) {
@@ -64,18 +56,36 @@ window.onload = function () {
                         .text(list[i].id));
             }
         });
-    }, 10000);
+    }
+
+    updateChannels();
+    //setInterval(updateChannels, 10000);
 
     $('#channelList').change(function() {
         curChannel = $('select[id=channelList]').val();
+        console.log(curChannel);
+        console.log(prevChannel);
         var registration = {
             registrationMessage: {
                 previousChannel: {id: prevChannel},
                 newChannel: {id: curChannel},
-                television: {id: "Web TV"}
+                television: {id: id}
             }
         };
         prevChannel = curChannel;
         socket.send(JSON.stringify(registration));
+    })
+
+    $('#send').change(function() {
+        var chime = {
+            chimeMessage : {
+                channel : {id: curChannel},
+                television : {id : id},
+                message : $('#send').val(),
+                timeSent : new Date().getTime().toString()
+            }
+        };
+        $('#send').val("");
+        socket.send(JSON.stringify(chime));
     })
 };
